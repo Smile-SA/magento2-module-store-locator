ef9591f7072283fed40c262d186390d4900255f8
Romain Ruaud <romain.ruaud@smile.fr>
2017-04-10 15:00:27 +0200

    Ensure markers distance is properly initialized on the map.

--- view/frontend/web/js/retailer/store-map.js
+++ view/frontend/web/js/retailer/store-map.js
@@ -20,7 +20,12 @@ define([
         initMarkers: function() {
             var markersList = new StoreCollection({items : this.markers});
             this.markers = markersList.getList();
-            this.displayedMarkers = ko.observable(markersList.getList());
+
+            this.markers.forEach(function(marker) {
+                marker.distance = ko.observable(0);
+            });
+
+            this.displayedMarkers = ko.observable(this.markers);
         }
 
     });
