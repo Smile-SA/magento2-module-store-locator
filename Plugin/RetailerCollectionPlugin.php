c618be0e22533c22bb5dbd80f2b8526cfaa40948
Romain Ruaud <romain.ruaud@smile.fr>
2017-07-10 15:19:10 +0200

    Fixing extensions attributes retrieval

--- Plugin/RetailerCollectionPlugin.php
+++ Plugin/RetailerCollectionPlugin.php
@@ -101,7 +101,9 @@ class RetailerCollectionPlugin
             foreach ($collection->getItems() as $currentItem) {
                 // Process hydrating Item data with the extension attributes Data.
                 $data = $this->joinProcessor->extractExtensionAttributes($entityType, $currentItem->getData());
-                $currentItem->setExtensionAttributes($data[ExtensibleDataInterface::EXTENSION_ATTRIBUTES_KEY]);
+                if ($data[ExtensibleDataInterface::EXTENSION_ATTRIBUTES_KEY]) {
+                    $currentItem->setExtensionAttributes($data[ExtensibleDataInterface::EXTENSION_ATTRIBUTES_KEY]);
+                }
 
                 if ($currentItem->getExtensionAttributes()->getAddress()) {
                     $currentItem->getExtensionAttributes()
